a20ab288a5ddc6d5fad1f357a19d2033a36ffebd
Alex <alex.podikoglou@gmail.com>
2024-08-08 07:34:07 +0300

    feat: parse return statements

--- src/parsing/statement_parser.rs
+++ src/parsing/statement_parser.rs
@@ -1,16 +1,33 @@
 use super::expression_parser::parse_expr;
-use crate::ir::statement::Statement;
+use crate::ir::{
+    expression::{Expression, Literal},
+    statement::Statement,
+};
 use anyhow::{bail, Result};
 use swc_ecma_ast::Stmt;
 
 /// Parses a [[Stmt] into an IR [[Statement]].
 pub fn parse_statement(statement: &Stmt) -> Result<Statement> {
     match statement {
-        Stmt::Expr(expr) => {
-            let parsed = parse_expr(&expr.expr)?;
+        Stmt::Expr(stmt) => {
+            let parsed = parse_expr(&stmt.expr)?;
 
             Ok(Statement::ExpressionStatement(parsed))
         }
+
+        Stmt::Return(stmt) => match &stmt.arg {
+            // parse expression & return it
+            Some(expr) => {
+                let parsed = parse_expr(&expr)?;
+
+                Ok(Statement::ReturnStatement(parsed))
+            }
+
+            // return void
+            None => Ok(Statement::ReturnStatement(Expression::Literal(
+                Literal::Void,
+            ))),
+        },
 
         other => bail!("non-supported statement kind: {:?}", other),
     }
