d2f9f3fc0f3f1a3cb2b20298847095498ff5c340
Alex <alex.podikoglou@gmail.com>
2024-08-11 03:31:11 +0300

    fix: add [] after param name for arrays

--- src/codegen/parameter.rs
+++ src/codegen/parameter.rs
@@ -1,4 +1,7 @@
-use crate::{def_codegen, ir::method::MethodParameter};
+use crate::{
+    def_codegen,
+    ir::{method::MethodParameter, types::Type},
+};
 
 def_codegen!(MethodParameter, |param| {
     let mut buffer = CodeBuffer::default();
@@ -6,6 +9,10 @@ def_codegen!(MethodParameter, |param| {
     buffer.write(param._type.to_c().unwrap());
     buffer.write(" ");
     buffer.write(param.name.as_str());
+
+    if matches!(&param._type, Type::Array(_)) {
+        buffer.write("[]");
+    }
 
     Ok(buffer)
 });
