c6416f72b8b1a54f2b42c1da1ad76a152d7d82e2
Alex <alex.podikoglou@gmail.com>
2024-08-12 10:05:21 +0300

    feat: don't write brackets on empty blocks

--- src/codegen/statement/block.rs
+++ src/codegen/statement/block.rs
@@ -3,13 +3,15 @@ use crate::{def_codegen, ir::statement::block::BlockStatement};
 def_codegen!(BlockStatement, |block| {
     let mut buffer = CodeBuffer::new();
 
-    buffer.write_line("{");
+    if !&block.0.is_empty() {
+        buffer.write_line("{");
 
-    for statement in &block.0 {
-        buffer.concat(&statement.to_c()?);
+        for statement in &block.0 {
+            buffer.concat(&statement.to_c()?);
+        }
+
+        buffer.write_line("}");
     }
-
-    buffer.write_line("}");
 
     Ok(buffer)
 });
