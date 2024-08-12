2f4519c92c2f999ecc5f4e58adee8033bb6f6efb
Alex <alex.podikoglou@gmail.com>
2024-08-12 10:06:32 +0300

    fix: warn instead of panic on non exported functions

--- src/parsing/statement/decl/mod.rs
+++ src/parsing/statement/decl/mod.rs
@@ -1,12 +1,21 @@
 pub mod var;
 
-use crate::{def_parser, ir::statement::Statement};
+use crate::{
+    def_parser,
+    ir::statement::{block::BlockStatement, Statement},
+};
 use anyhow::bail;
 use swc_ecma_ast::Decl;
 
 def_parser!(Decl, Statement, |decl| {
     match decl {
         Decl::Var(decl) => decl.to_ir(),
+        Decl::Fn(_) => {
+            eprintln!("warning: you have non exported functions. They will not be transpiled.");
+
+            // this is not written since it's an empty block
+            Ok(Statement::Block(BlockStatement(vec![])))
+        }
 
         other => bail!("non-supported declaration kind: {:?}", other),
     }
